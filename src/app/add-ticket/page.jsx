c13a115b8707c229f33f6dd0288bf7e1443557df
Adis Giulbudagiants <adisforwork2001@gmail.com>
2024-04-04 22:29:14 +0400

    Make a Navbar and TicketsList componennts

---
+++ src/app/add-ticket/page.jsx
@@ -0,0 +1,5 @@
+const AddTicket = () => {
+  return <div>page</div>
+}
+
+export default AddTicket
---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import TicketsList from '@/components/TicketsList'
+
+export default function Home() {
+  return (
+    <main>
+      <TicketsList />
+    </main>
+  )
+}
